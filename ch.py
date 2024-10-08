fd45bcc3f2b29a5223bd27208ecefb02821f53b6
husnainidrees <husnainidrees78@gmail.com>
2024-10-08 16:17:17 +0500

    now we enter the ch.py files

---
+++ ch.py
@@ -0,0 +1,22 @@
+
+
+
+def char_inde(str):
+
+    res=str[0]
+    x=len(str/2)
+
+    mi=int(x/2)
+
+    res=res+ str[mi]
+    res=res + str(len(str)-1)
+
+
+
+
+
+
+
+str1="geekfor"
+
+print(char_inde(str1))
